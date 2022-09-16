9a3d9f2e51ba383a29d275be3e1ebd38197dc66a
golovkinen <golovkinen@yandex.ru>
2022-09-16 15:09:13 +0300

    Refactor: add-controllers

---
+++ src/main/java/ru/practicum/shareit/item/model/IItemService.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit.item.model;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IItemService {
+    Item create (Item item, int userId);
+    List<Item> readAll ();
+    List<Item> readAllUserItems (int id);
+    Optional<Item> read (int id);
+    boolean update(Item item, int userId, int itemId);
+    boolean delete(int id, int userId);
+    boolean deleteAllUserItems(int id);
+    List<Item> searchItemByWord(String searchSentence);
+}
---
+++ src/main/java/ru/practicum/shareit/user/IUserRepository.java
@@ -0,0 +1,10 @@
+package ru.practicum.shareit.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail (String email);
+}
