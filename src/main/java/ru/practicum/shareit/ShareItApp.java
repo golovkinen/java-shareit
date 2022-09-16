8f78df4dd6c889fc51f4a564b9171fb6c960b0b5
golovkinen <golovkinen@yandex.ru>
2022-09-16 17:17:16 +0300

    Refactor: add-controllers

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ShareItApp {
---
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -0,0 +1,46 @@
+package ru.practicum.shareit.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService implements IUserService{
+
+    private final IUserRepository iUserRepository;
+    private final IUserRepositoryCustom iUserRepositoryCustom;
+
+    public UserService(IUserRepository iUserRepository, IUserRepositoryCustom iUserRepositoryCustom) {
+        this.iUserRepository = iUserRepository;
+        this.iUserRepositoryCustom = iUserRepositoryCustom;
+    }
+
+    @Override
+    public User create(User user) {
+        if (iUserRepository.findByEmail(user.getEmail()).isPresent()) {
+            return null;
+        }
+        return iUserRepository.save(user);
+    }
+
+    @Override
+    public List<User> readAll() {
+        return iUserRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> read(int id) {
+        return iUserRepository.findById(id);
+    }
+
+    @Override
+    public boolean update(User user, int userId) {
+        return iUserRepositoryCustom.updateUser(user, userId);
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return iUserRepositoryCustom.deleteById(id);
+    }
+}
