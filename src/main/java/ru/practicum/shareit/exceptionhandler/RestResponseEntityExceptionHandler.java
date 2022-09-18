4a235c2815bb4b384826f423a8b1c5aa1ce5288f
golovkinen <golovkinen@yandex.ru>
2022-09-18 16:09:11 +0400

    Refactor: add-controllers

---
+++ src/main/java/ru/practicum/shareit/exceptionhandler/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.exceptionhandler;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler
+        extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value
+            = {ConstraintViolationException.class})
+    protected ResponseEntity<Object> handleConflict(
+            RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "This should be application specific";
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
