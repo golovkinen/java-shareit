46610bba040ee2bb4897c41f52fba77a5deb8257
golovkinen <golovkinen@yandex.ru>
2022-09-18 22:16:24 +0400

    Refactor: add-controllers

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,51 @@
 package ru.practicum.shareit.item.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.IndexedEmbedded;
+import ru.practicum.shareit.annotation.TrueOrFalse;
+import ru.practicum.shareit.user.User;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Indexed
+@Table(name = "ITEMS")
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ITEM_ID", nullable = false)
+    private Integer id;
+    @Column(name = "ITEM_NAME", nullable = false)
+    @NonNull
+    @NotBlank
+    @FullTextField
+    private String name;
+    @Column(name = "ITEM_DESCRIPTION", nullable = false)
+    @NonNull
+    @NotBlank
+    @Size(max = 200)
+    @FullTextField
+    private String description;
+    @Column(name = "ITEM_AVAILABLE")
+    @NonNull
+    @TrueOrFalse
+    @GenericField
+    private Boolean available;
+    @JsonIgnore
+    @ManyToOne
+    @IndexedEmbedded
+    private User user;
 }
